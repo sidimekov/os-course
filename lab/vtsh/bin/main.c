7838e662ba3e256ddcde459d6840a6d10fb72b80
sidimekov <dimasidim8582@gmail.com>
2025-10-24 01:26:16 +0300

    refactor: clang-tidy fix for main.c

--- lab/vtsh/bin/main.c
+++ lab/vtsh/bin/main.c
@@ -2,24 +2,30 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <sys/types.h>
 
 #include "vtsh.h"
 
 int main(void) {
-  setvbuf(stdin, NULL, _IONBF, 0);
+  if (setvbuf(stdin, NULL, _IONBF, 0) != 0) {
+    perror("setvbuf(stdin)");
+    return EXIT_FAILURE;
+  }
 
   char* line = NULL;
   size_t cap = 0;
 
-  while (1) {
+  for (;;) {
     vtsh_print_prompt();
-    ssize_t n = getline(&line, &cap, stdin);
-    if (n < 0)
+    ssize_t n_line = getline(&line, &cap, stdin);
+    if (n_line < 0) {
       break;
+    }
 
-    if (vtsh_execute_line(line) < 0)
+    if (vtsh_execute_line(line) < 0) {
       break;  // exit
+    }
   }
   free(line);
-  return 0;
+  return EXIT_SUCCESS;
 }
